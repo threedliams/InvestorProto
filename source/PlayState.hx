57eb504d5f98a4572e527002ecaa0957d1a62833
threedliams <threedliams@gmail.com>
2017-09-17 19:09:15 -0400

    Revert "initial commit"
    This reverts commit 25731c54f6083c6eb37f9a5eca2162a96a794b60.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -13,7 +13,6 @@ class PlayState extends FlxState
 	private var cashText:FlxText;
 	private var investmentText:FlxText;
 	
-	
 	override public function create():Void
 	{
 		board = new BoardGrid(0, 0, 15, 15, this);
@@ -27,7 +26,6 @@ class PlayState extends FlxState
 		
 		investmentText = new FlxText(Std.int(board.x) + board.getBoardPixelWidth() + 20, 80, 500, "Investment value: $" + investments, 30);
 		add(investmentText);
-		
 		
 		super.create();
 	}
